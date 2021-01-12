44dc3555d6a4cb76c4e5f6eabb668876b9eb0dfe
Daniel Lessenden <dless27@gmail.com>
2021-01-12 13:40:47 -0600

    Initial commit

---
+++ script1.py
@@ -0,0 +1,17 @@
+"""
+A program that stores this book informaiton
+
+title
+author
+year
+isbn
+
+user can view all records
+search an entry 
+add entry 
+update entry
+delete entry 
+close program
+"""
+
+
